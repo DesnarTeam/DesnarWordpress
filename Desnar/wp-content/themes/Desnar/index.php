e585bece6e9c02dcda7bbede861deb1d66391ec3
Nitin Patel <nrsdpatel@gmail.com>
2016-11-16 11:20:33 +0000

    index changed

--- Desnar/wp-content/themes/Desnar/index.php
+++ Desnar/wp-content/themes/Desnar/index.php
@@ -40,7 +40,8 @@
                     <div class="entry-utility">
                         <span class="cat-links"><span class="entry-utility-prep entry-utility-prep-cat-links"><?php _e( 'Posted in ', 'Desnar' ); ?></span><?php echo get_the_category_list(', '); ?></span>
                         <span class="meta-sep"> | </span>
-                        <?php the_tags( '<span class="tag-links"><span class="entry-utility-prep entry-utility-prep-tag-links">' . __('Tagged ', 'Desnar' ) . '</span>', ", ", "</span><span class="meta-sep">|</span>" ) ?>
+                         
+                        <?php the_tags( '<span class="tag-links"><span class="entry-utility-prep entry-utility-prep-tag-links">' . __('Tagged ', 'Desnar' ) . '</span>', ", ", "</span>\n\t\t\t\t\t\t<span class=\"meta-sep\">|</span>\n" ) ?>
                         <span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'Desnar' ), __( '1 Comment', 'Desnar' ), __( '% Comments', 'Desnar' ) ) ?></span>
                         <?php edit_post_link( __( 'Edit', 'Desnar' ), '<span class="meta-sep">|</span><span class="edit-link">', '</span>' ); ?>
                     </div><!-- #entry-utility -->
